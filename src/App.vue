9afc13f67aa3ea271d4017b767201b32448d284a
Lama Sonmez <lamasonmez.maids.cc@gmail.com>
2021-10-08 22:29:32 +0300

    style fixes

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
 </script>
 
 <style >
-
+@import "assets/sass/app.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/page-editor/page-editor.view.vue
+++ src/views/page-editor/page-editor.view.vue
@@ -2,12 +2,12 @@
   <div class="container-fluid mt-3">
     <div class="col-12">
       <div class="d-flex justify-content-end w-100">
-        <div class="select-container mr-3">
+        <div class="select-container ms-3">
           <select class="form-control" id="templatesHistory">
             <option :value="item.id" v-for="item in templates " :key="item.id">{{ item.name }}</option>
           </select>
         </div>
-        <button class="btn btn-success" data-toggle="modal" data-target="#saveTemplate">Save</button>
+        <button class="btn btn-success ms-3" data-toggle="modal" data-target="#saveTemplate">Save</button>
       </div>
     </div>
     <previewComponent></previewComponent>
